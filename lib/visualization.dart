b096a7c237251d8f0ae66fb457fba35b32f29033
David Langheiter <david@langheiter.com>
2019-12-06 13:36:28 +0100

    Added eq methods

--- lib/visualization.dart
+++ lib/visualization.dart
@@ -6,6 +6,16 @@ class ClockVisualizationSettings {
   ClockVisualizationSettings({
     this.bgColor,
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ClockVisualizationSettings &&
+          runtimeType == other.runtimeType &&
+          bgColor == other.bgColor;
+
+  @override
+  int get hashCode => bgColor.hashCode;
 }
 
 abstract class ClockVisualization {
@@ -15,6 +25,17 @@ abstract class ClockVisualization {
   ClockVisualization(this.settings);
 
   CustomPainter getCustomPainter();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ClockVisualization &&
+          runtimeType == other.runtimeType &&
+          time == other.time &&
+          settings == other.settings;
+
+  @override
+  int get hashCode => time.hashCode ^ settings.hashCode;
 }
 
 class VisualizationSettings {
@@ -25,6 +46,17 @@ class VisualizationSettings {
     @required this.name,
     this.refreshTime = const Duration(milliseconds: 0),
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VisualizationSettings &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          refreshTime == other.refreshTime;
+
+  @override
+  int get hashCode => name.hashCode ^ refreshTime.hashCode;
 }
 
 class Visualization {
@@ -32,4 +64,15 @@ class Visualization {
   final ClockVisualization visualization;
 
   Visualization(this.settings, this.visualization);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Visualization &&
+          runtimeType == other.runtimeType &&
+          settings == other.settings &&
+          visualization == other.visualization;
+
+  @override
+  int get hashCode => settings.hashCode ^ visualization.hashCode;
 }
