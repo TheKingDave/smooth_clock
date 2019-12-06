2c7cbc01d691ae9977bc437686398145981bc4b1
David Langheiter <david@langheiter.com>
2019-12-06 13:36:42 +0100

    Fixed problem with changing visulization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smoothclock/toSwatch.dart';
 import 'package:flutter/services.dart';
+import 'package:smoothclock/visu/BinaryClock.dart';
 import 'package:smoothclock/visu/SmoothClock.dart';
 import 'package:smoothclock/visualization.dart';
 
@@ -13,11 +14,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final ClockVisualizationSettings vss =
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  static final ClockVisualizationSettings vss =
       ClockVisualizationSettings(bgColor: Colors.black);
 
-  // This widget is the root of your application.
+  static final List<Visualization> visualizations = [
+    Visualization(
+        VisualizationSettings(
+            name: "Smooth Clock",
+            refreshTime: const Duration(milliseconds: 100)),
+        SmoothClock(vss)),
+    Visualization(
+        VisualizationSettings(
+            name: "Binary Clock",
+            refreshTime: const Duration(milliseconds: 100)),
+        BinaryClock(vss))
+  ];
+
+  Visualization selected;
+  Key _key;
+
+  @override
+  void initState() {
+    super.initState();
+    selected = visualizations.first;
+    _key = UniqueKey();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,12 +57,25 @@ class MyApp extends StatelessWidget {
         primarySwatch: toSwatch(Colors.black),
       ),
       home: HomePage(
-        title: 'Smooth Clock',
-        child: Clock(Visualization(
-            VisualizationSettings(
-                name: "Smooth", refreshTime: const Duration(milliseconds: 1)),
-            SmoothClock(vss))),
-      ),
+          title: selected.settings.name,
+          child: Clock(selected, key: _key),
+          clocks: List.from(visualizations.map((v) => v.settings.name)),
+          selected: selected.settings.name,
+          onSelect: (name) {
+            Visualization newSelect;
+            for(var v in visualizations) {
+              if(v.settings.name == name) {
+                newSelect = v;
+                break;
+              }
+            }
+            if(newSelect != null && newSelect != selected) {
+              setState(() {
+                selected = newSelect;
+                _key = UniqueKey();
+              });
+            }
+          }),
     );
   }
 }
@@ -40,30 +83,41 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   final String title;
   final Widget child;
+  final List<String> clocks;
+  final String selected;
+  final Function(String name) onSelect;
 
-  HomePage({this.title, this.child, Key key}) : super(key: key);
+  HomePage(
+      {this.title,
+      this.child,
+      this.clocks,
+      this.selected,
+      this.onSelect,
+      Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = [
+      ListTile(
+        title: Text("Clock Styles", style: TextStyle(fontSize: 20)),
+      ),
+      Divider(),
+    ];
+
+    children.addAll(clocks.map((name) => ListTile(
+          title: Text(name),
+          selected: this.selected == name,
+          onTap: () => onSelect(name),
+        )));
+
     return Scaffold(
       appBar: AppBar(
         title: Text(this.title),
       ),
       drawer: Drawer(
         child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text("Clock Styles", style: TextStyle(fontSize: 20)),
-            ),
-            Divider(),
-            ListTile(
-              title: Text("Smooth Clock"),
-              selected: true,
-            ),
-            ListTile(
-              title: Text("Binary Clock"),
-            ),
-          ],
+          children: children,
         ),
       ),
       body: child,
